c6d71a7a517dec67c1b8c0bb4c80bc65d4ac9e05
abate2283 <alfredbate@yahoo.co.uk>
2023-04-24 15:14:53 -0500

    commented out code from branch

--- 2. Inside Classes/exercises/Cat.java
+++ 2. Inside Classes/exercises/Cat.java
@@ -32,16 +32,16 @@ public class Cat {
 
 	public static void main(String[] args) {
 		/* Do the following things without changing the Cat class */
-		Cat cat = new Cat("ManBoy");
+//		Cat cat = new Cat("ManBoy");
 
 		// 1. Make the Cat meow
-		cat.meow();
+//		cat.meow();
 
 		// 2. Get the Cat to print it's name
-		cat.printName();
+//		cat.printName();
 
 		// 3. Kill the Cat!
-		cat.kill(-20);
+//		cat.kill(-20);
 
 	}
 }
